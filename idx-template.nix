b61b7f72dcbe8bbbf751cabdccff4ee2e5a7110f
lingkai <slk49@live.cn>
2023-12-13 17:00:15 -0500

    Add bootstraping project id

--- idx-template.nix
+++ idx-template.nix
@@ -1,7 +1,8 @@
-{ pkgs, ... }: {
+{ pkgs, projectId, ... }: {
   bootstrap = ''
     cp -rf ${./.}/app "$WS_NAME"
     chmod -R +w "$WS_NAME"
+    sed 's/<project-id>/${projectId}/' app/.firebaserc.template > app/.firebaserc
     mv "$WS_NAME" "$out"
   '';
 }
