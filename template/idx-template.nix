aee399716c4bb84f5fb737124aadf259a815e098
lingkai <lingkai@google.com>
2024-06-12 17:21:47 +0000

    Add apikey txt

--- template/idx-template.nix
+++ template/idx-template.nix
@@ -4,6 +4,7 @@
     cp -rf ${./app}/. "$out"
     chmod -R +w "$out"
     sed 's/<project-id>/${projectId}/' ${./app}/.firebaserc > "$out"/.firebaserc
+    echo ${apiKey} > "$out/apikey.txt"
     mkdir -p "$out"/.idx
     sed -e 's/<project-id>/${projectId}/' -e 's/<api-key>/${apiKey}/' ${./dev.nix} > "$out/.idx/dev.nix"
   '';
