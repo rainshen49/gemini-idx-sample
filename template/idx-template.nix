3122bf0c038730b586c5aa9f7995f38be190b222
lingkai <lingkai@google.com>
2024-08-02 20:42:31 +0000

    Cache flutter sdk to improve download time

--- template/idx-template.nix
+++ template/idx-template.nix
@@ -1,6 +1,13 @@
-{ pkgs, projectId, ... }: {
+{ pkgs, projectId, ... }:
+let
+  flutter_sdk = pkgs.fetchurl {
+    url = "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.3-stable.tar.xz";
+    sha256 = "sha256-nF9wuhGLkWNVIUSQGi79kdQLIqaKBOZycdalrZNug2g=";
+    name = "flutter_linux_3.22.3-stable.tar.xz";
+  };
+in
+{
   packages = [
-    pkgs.curl
     pkgs.gnutar
     pkgs.xz
   ];
@@ -11,8 +18,6 @@
     # Apply project ID to configs
     sed -e 's/<project-id>/${projectId}/' ${./app/.idx/dev.nix} > "$out/.idx/dev.nix"
     # Install the flutter SDK
-    curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.3-stable.tar.xz
-    tar -xf "./flutter_linux_3.22.3-stable.tar.xz" -C "$out/.idx"
-    rm ./flutter_linux_3.22.3-stable.tar.xz
+    tar -xf ${flutter_sdk} -C "$out/.idx"
   '';
 }
